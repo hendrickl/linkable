857df27be074ad33dac3dadad841e90d9f71ec64
hendrick <h.lincertin@gmail.com>
2020-12-07 11:38:52 +0100

    add web3-hooks

--- src/components/AccessPage.js
+++ src/components/AccessPage.js
@@ -2,10 +2,8 @@ import React from "react";
 import { ethers } from "ethers";
 
 const AccessPAge = () => {
-  // Interface d'accueil simple invitant à se connecter
-  // sur MetaMask
+  // Interface d'accueil simple invitant à se connecter sur MetaMask
+  // Une fois la connection faite, ouverture de MainPage
 
-  // Check isWeb3
-  // Si MetaMask
   return <MainPage />;
 };
