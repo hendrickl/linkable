e79d48c1b206cf234b460533f8d75805ffc71bd3
hendrick <h.lincertin@gmail.com>
2020-12-07 21:21:30 +0100

    index.js : Web3Provider hook

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Web3Provider } from "web3-hooks";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3Provider>
+      <App />
+    </Web3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
+//  contract address: 0x58D68c269Be27375D567783BCF215679d12805B4
+//  account: 0xbeA1B719eE1d279762412E8282a032B0d86204C7
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
